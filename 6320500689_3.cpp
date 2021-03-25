53fa1f2b4b5d81f342c98ef7f858671d2f1d8492
b6320500689 <supitchaya.r@ku.th>
2021-03-25 17:55:40 +0700

    333

--- 6320500689_3.cpp
+++ 6320500689_3.cpp
@@ -1,12 +1,28 @@
 #include <stdio.h>
+#include <string.h>
 int main ()
 {
-	int n;
+	int n,m=0,k=2;
+	int nn = n;
 	scanf ("%d",&n);
-	if (n>=1 && n<=1000000)
-	{
-		
-	}
-	printf ("%d",n);
+	//if (n>=1 && n<=1000000)
+		while (nn>0)
+		{
+			int d= nn%10;
+			nn = nn/10;
+			m = m*10 + d;
+		}
+		int p = (m==n);
+		int prime=1;
+		while (k<n)
+		{
+			if (n%k == 0)
+			p=0;
+			break;
+		}
+		k++;
+		if (p && prime)
+		printf ("%d\n",n);
+		n++;
 	return 0;
 }
