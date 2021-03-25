2c4c63907f9880e4171cebdc743c298640fa5da3
b6320500689 <supitchaya.r@ku.th>
2021-03-25 18:06:17 +0700

    3333

--- 6320500689_3.cpp
+++ 6320500689_3.cpp
@@ -2,27 +2,22 @@
 #include <string.h>
 int main ()
 {
-	int n,m=0,k=2;
+	int n,m=0,t;
 	int nn = n;
 	scanf ("%d",&n);
-	//if (n>=1 && n<=1000000)
-		while (nn>0)
+	if (n>=1 && n<=1000000)
+	{
+		t=n;
+		while (t!=0)
 		{
-			int d= nn%10;
-			nn = nn/10;
-			m = m*10 + d;
+			m = m*10;
+			m = m+t%10;
+			t = t/10;
 		}
-		int p = (m==n);
-		int prime=1;
-		while (k<n)
-		{
-			if (n%k == 0)
-			p=0;
-			break;
 		}
-		k++;
-		if (p && prime)
-		printf ("%d\n",n);
-		n++;
+	if (n == m)
+	printf ("%d is a palindrome\n",n);
+	else
+	printf ("%d is not a palindrome\n",n);
 	return 0;
 }
