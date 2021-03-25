ceda548f5d5660a7e778495770e6f03405536473
b6320500689 <supitchaya.r@ku.th>
2021-03-25 18:16:17 +0700

    111

--- 6320500689_1.cpp
+++ 6320500689_1.cpp
@@ -1,12 +1,22 @@
 #include <stdio.h>
 int main ()
 {
-	int n;
+	int n,i;
 	scanf ("%d",&n);
 	if (n>=1 && n<=1000000)
 	{
-		
+		while (n>1)
+		{
+			for (i=2; i<=n; i++)
+			{
+				if (n%i == 0)
+				{
+					printf ("%d ",i);
+					n =n/i;
+					break;
+				}
+			}
+		}
 	}
-	printf ("%d",n);
 	return 0;
 }
